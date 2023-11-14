5dc6be05477a5374af2ed3e97b4dd0b9aee3544a
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-13 21:47:00 -0600

    Creando clase circulo

---
+++ EjercicioPolimorfismo/Circulo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+{
+    public class Circulo : Figura
+    {
+        double Radio { get; set; }
+
+        public Circulo(double radio) {        
+            Radio = radio;  
+        }
+
+        public override double CalcularArea()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 2 * Math.PI * Radio;
+        }
+    }
+}
