b78aed20668fb1bba5441ac8b43f670403f9cd9a
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-13 21:49:57 -0600

    creando Clase Rectangulo

---
+++ EjercicioPolimorfismo/Rectangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+{
+    public class Rectangulo : Figura
+    {
+
+        double LadoA {  get; set; }
+        double LadoB { get; set; }
+
+        public Rectangulo(double ladoA, double ladoB) {
+            LadoA = ladoA;
+            LadoB = ladoB;        
+        }
+
+        public override double CalcularArea()
+        {
+            return LadoA * LadoB;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return 2 * (LadoA + LadoB);
+        }
+    }
+}
