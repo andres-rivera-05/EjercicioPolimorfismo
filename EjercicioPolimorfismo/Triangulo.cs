9b5c97963497261759478174655e89f0bfca447b
andres-rivera-05 <cristianrivera161141@gmail.com>
2023-11-13 21:55:54 -0600

    Creando clase triangulo

---
+++ EjercicioPolimorfismo/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioPolimorfismo
+{
+    public class Triangulo : Figura
+    {
+
+
+        double LadoA {  get; set; } 
+        double LadoB { get; set; }  
+        double LadoC {  get; set; } 
+
+
+        public Triangulo(double ladoA, double ladoB, double ladoC) { 
+          LadoA = ladoA;    
+          LadoB = ladoB;  
+          LadoC = ladoC;
+        
+        }  
+
+        public override double CalcularArea()
+        {
+
+            double cal = (LadoA + LadoB + LadoC) / 2;
+            return Math.Sqrt(cal *(cal - LadoA) * (cal -LadoB) * (cal - LadoC));  
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return LadoA + LadoB + LadoC;
+        }
+    }
+}
